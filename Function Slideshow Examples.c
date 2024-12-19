64a0948568f9cbf98daeac37b11d3e0ab3b33ed0
CardsAndGamesYT <74144694+CardsAndGamesYT@users.noreply.github.com>
2024-12-19 13:50:08 -0500

    Update Function Slideshow Examples.c

--- Function Slideshow Examples.c
+++ Function Slideshow Examples.c
@@ -7,5 +7,14 @@ task main()
 
 void ExampleFunction(int NumVar, bool BoolVar)
 {
-  
+  if(BoolVar == 1)
+  {
+    motor(M_Motor_left) = NumVar;
+    wait1Msec(1000);
+  }
+  else
+  {
+    motor(M_Motor_Right) = NumVar;
+    wait1Msec(1000);
+  }
 }
