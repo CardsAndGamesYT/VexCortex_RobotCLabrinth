f84aff97b60dd1afecc684d3bf2d9ecf722367a8
CardsAndGamesYT <74144694+CardsAndGamesYT@users.noreply.github.com>
2024-12-20 12:27:01 -0500

    Create Auto Straight Example W-Function.c

---
+++ Auto Straight Example W-Function.c
@@ -0,0 +1,30 @@
+void DriveStraight(int FastSpeed, int SlowSpeed, int cts);
+
+task mian()
+{
+  
+}
+
+void DriveStraight(int FastSpeed, int SlowSpeed, int cts)
+{
+  resetMotorEncoder(M_Motor_Left);
+	resetMotorEncoder(M_Motor_Right);
+	while(getMotorEncoder(M_Motor_Left) < cts)
+	{
+		if(getMotorEncoder(M_Motor_Right) > getMotorEncoder(M_Motor_Left))
+		{
+			motor(M_Motor_Left) = FastSpeed;
+			motor(M_Motor_Right) = SlowSpeed;
+		}
+		if(getMotorEncoder(M_Motor_Right) < getMotorEncoder(M_Motor_Left))
+		{
+			motor(M_Motor_Left) = SlowSpeed;
+			motor(M_Motor_Right) = FastSpeed;
+		}
+		if(getMotorEncoder(M_Motor_Right) == getMotorEncoder(M_Motor_Left))
+		{
+			motor(M_Motor_Left) = SlowSpeed;
+			motor(M_Motor_Right) = SlowSpeed;
+		}
+	}
+}
